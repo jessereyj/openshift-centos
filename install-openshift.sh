193a2b63300ac6b685a1b3bcb00990a39f873321
Jebong <jessereyj@gmail.com>
2020-06-24 13:41:41 +0000

    Use sudo

--- install-openshift.sh
+++ install-openshift.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p /etc/origin/master/
-touch /etc/origin/master/htpasswd
+sudo mkdir -p /etc/origin/master/
+sudo touch /etc/origin/master/htpasswd
 ## Default variables to use
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-3.11 --depth=1
 
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+sudo ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
+sudo ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 
-htpasswd -b /etc/origin/master/htpasswd jebong 214jebong123
+sudo htpasswd -b /etc/origin/master/htpasswd jebong 214jebong123
 oc adm policy add-cluster-role-to-user cluster-admin jebong
